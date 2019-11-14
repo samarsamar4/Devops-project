e9f2d784ad43f46433979f1c42d087f99054bd09
Samar CHEBBI <47428456+samarsamar4@users.noreply.github.com>
2019-11-14 10:09:09 +0100

    Add files via upload

---
+++ Deployment-flask-master/static/js/main.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function(){
+    "use strict";
+$('#slider-carousel').carouFredSel({
+    responsive:true,
+    width:'100%',
+    circular:true,
+    scroll:{
+        Items:1,
+        duration:500,
+        pauseOnHover:true,
+    },
+    auto:true,
+    items:{
+        visible:{
+            min:1,
+            max:1
+        },
+        height:"variable"
+    },
+    pagination:{
+        container:".sliderpager",
+        pageAnchorBuilder:false,
+    }
+})
+})
